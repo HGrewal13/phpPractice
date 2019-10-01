e0128a113f2ddb638567a187b1e1fef429d86bdb
Harry <harrysgrewal13@gmail.com>
2019-10-01 15:20:27 -0400

    basic madlibs and array

--- site.php
+++ site.php
@@ -7,9 +7,53 @@
     <title>Document</title>
 </head>
 <body>
-    
+
+    <!-- // $phrase = 'Giraffe Academy';
+    // echo substr($phrase, 8, 3); -->
+
+    <!-- <form action="site.php" method="get">
+        Name: <input type="text" name="name">
+        Age: <input type="number" name="age">
+        <input type="submit">
+    </form>
+    <br>
+
+    Your name is <?php echo $_GET["name"] ?>
+    <br>
+    Your age is <?php echo $_GET['age'] ?> -->
+
+    <!-- <form action="site.php" method="get">
+        <input type="number" name='num1'>
+        <input type="number" name='num2'>
+        <input type="submit">
+    </form>
+        
+    <?php echo $_GET['num1'] + $_GET['num2'] ?> -->
+
+<!-- MAD LIBS -->
+    <!-- <form action="site.php" method="get">
+        Color: <input type="text" name="color"><br>
+        Plural Noun: <input type="text" name="pluralNoun"><br>
+        Celebrity: <input type="text" name="celebrity"><br>
+        <input type="submit">
+    </form>
+    <br><br>
     <?php
-        echo("Hello World");
+        $color = $_GET['color'];
+        $pluralNoun = $_GET['pluralNoun'];
+        $celebrity = $_GET['celebrity'];
+        echo "Roses are $color <br>";
+        echo "$pluralNoun are blue <br>";
+        echo "I love $celebrity <br>";
+    ?> -->
+
+<!-- Arrays -->
+    <?php 
+        $friends = array('kevin', 'karen', 'oscar', 'jim');
+        $friends[4] = "Dwight";
+        echo count($friends);
     ?>
+
+
 </body>
 </html>
